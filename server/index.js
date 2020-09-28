4648c2fb7506e5ce8be4906db7b460294f4d891f
Lunaticsatoshi <deysarkersudeep@gmail.com>
2020-09-28 21:23:19 +0530

    :sparkles: added middleware support

--- server/index.js
+++ server/index.js
@@ -1,15 +1,27 @@
 require('dotenv').config;
 const express = require('express');
+const morgan = require('morgan');
 
 
 const app = express();
 
-app.get('/waifus', (req,res) => {
-    res.json({
+//Get All resturants
+app.get('/api/v1/getWaifus', (req,res) => {
+    res.status(200).json({
         status: 'sucess',
-        waifu: 'shinobu',
+        data: {
+            waifu: ['shinobu', 'Mai'],
+        }
     })
+});
+
+//Get a Single Waifu
+app.get('/api/v1/getWaifus/:waifuid', (req,res) => {
+    console.log(req.params.waifuid)
 })
+
+//Add A waifu
+app.post('api/v1/waifus', (req,res) => {})
 
 const port = process.env.PORT || 3004;
 app.listen(port, () => {
